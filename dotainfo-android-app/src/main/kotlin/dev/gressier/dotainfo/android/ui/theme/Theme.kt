729d35e1abbab33e3ba31e527bccfb5612cbb17f
Alexandre Gressier <alexandre@gressier.dev>
2022-05-01 16:56:45 +0200

    Define the design system

--- dotainfo-android-app/src/main/kotlin/dev/gressier/dotainfo/android/ui/theme/Theme.kt
+++ dotainfo-android-app/src/main/kotlin/dev/gressier/dotainfo/android/ui/theme/Theme.kt
@@ -1,21 +1,42 @@
 package dev.gressier.dotainfo.android.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
+@SuppressLint("ConflictingOnColor")
 private val lightColorScheme = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Black1,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
+    onSecondary = Color.Black,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = Grey1,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Black1,
 )
 
 private val darkColorScheme = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = Blue300,
+    primaryVariant = Blue700,
+    onPrimary = Color.White,
+    secondary = Color.Black,
+    secondaryVariant = Teal300,
+    onSecondary = Color.White,
+    error = RedErrorLight,
+    onError = RedErrorDark,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = Black1,
+    onSurface = Color.White,
 )
 
 @Composable
--- dotainfo-android-app/src/main/kotlin/dev/gressier/dotainfo/android/ui/theme/Typograhy.kt
+++ dotainfo-android-app/src/main/kotlin/dev/gressier/dotainfo/android/ui/theme/Typograhy.kt
@@ -1,5 +1,85 @@
 package dev.gressier.dotainfo.android.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import dev.gressier.dotainfo.android.R
 
-val typography = Typography()
+private val QuickSand = FontFamily(
+    Font(R.font.quicksand_light, FontWeight.Light),
+    Font(R.font.quicksand_regular, FontWeight.Normal),
+    Font(R.font.quicksand_medium, FontWeight.Medium),
+    Font(R.font.quicksand_bold, FontWeight.Bold),
+)
+
+val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Medium,
+        fontSize = 32.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Medium,
+        fontSize = 26.sp,
+    ),
+    h3 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
+    ),
+    h4 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+    ),
+    h5 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+    ),
+    h6 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+    ),
+    body1 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+    ),
+    body2 = TextStyle(
+        fontFamily = QuickSand,
+        fontSize = 14.sp,
+    ),
+    button = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        color = Color.White,
+    ),
+    caption = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+    ),
+    overline = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+    ),
+)
